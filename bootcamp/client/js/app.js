cb85216ac3a6d3001edfa0a7e22c6f3aafe33640
Nicolai Gheorghiev <ngheorghiev@gmail.com>
2017-09-19 00:37:25 +0300

    add styles

--- bootcamp/client/js/app.js
+++ bootcamp/client/js/app.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import '../scss/styles.scss';
 
 class PersonProfile extends React.Component {
     render() {
@@ -8,19 +9,20 @@ class PersonProfile extends React.Component {
         // Third element is the content.
         //return React.createElement('h1', null, 'PersonProfile');
         return <article>
-            <h1> Full Name: Lucy Chang </h1>
-            <p>
-                <img src='images/LucyChang.jpg'/>
-            </p>
-            <span>Birthday: <time dateTime="12-31"><img src='images/capricorn.png' height='20px' weight="'20px"/> 12/31</time></span>
+            <h1 className="pageTile"> Full Name: Lucy Chang </h1>
+            <img className="borderBlink" src='images/LucyChang.jpg'/>
+            <br/>
+            <span>Birthday: <time dateTime="12-31"><img src='images/capricorn.png' height='20px' weight='20px'/> 12/31</time></span>
+            <div>
             <p>Three interesting things:</p>
             <ul>
-                <li> Been to 15 national parks.</li>
+                <li className={'critical'}> Been to 15 national parks.</li>
                 <li>
                     <mark>Never had diet coke.</mark>
                 </li>
                 <li>More than 3 years in Intuit</li>
             </ul>
+            </div>
         </article>;
     }
 }
