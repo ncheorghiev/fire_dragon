d88bf2fe766ced18e9964f173c4450b8ae0ae500
Nicolai Gheorghiev <ngheorghiev@gmail.com>
2019-01-05 23:26:46 +0300

    Functional for component

--- resafa/src/components/Person.js
+++ resafa/src/components/Person.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import '../App.css';
 
-class Person extends Component {
-    render() {
-        return (
-            <p align="left">
-                {this.props.person.name} is {this.props.person.age} and {this.props.person.sex === 'Male' ? 'he' : 'she' } is a {this.props.person.sex}
-            </p>
-        );
-    }
-}
+
+// Functional way to present simple HTML
+let Person = (props) => (
+    <p align="left">
+        {props.person.name} is {props.person.age} and {props.person.sex === 'Male' ? 'he' : 'she' } is a {props.person.sex}
+    </p>
+);
+
+// class Person extends Component {
+//     render() {
+//         return (
+//             <p align="left">
+//                 {this.props.person.name} is {this.props.person.age} and {this.props.person.sex === 'Male' ? 'he' : 'she' } is a {this.props.person.sex}
+//             </p>
+//         );
+//     }
+// }
 
 export default Person;
