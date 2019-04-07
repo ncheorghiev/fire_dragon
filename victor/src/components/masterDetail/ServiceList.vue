8c9ff97cfd596ee0edf3390dd880357023a8506e
Nicolai Gheorghiev <ngheorghiev@gmail.com>
2019-04-07 08:02:20 +0300

    Add master detail view

---
+++ victor/src/components/masterDetail/ServiceList.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <table>
+    <thead>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Time</th>
+      <th>Rate</th>
+      <th>limit</th>
+    </thead>
+    <template v-for="service in services">
+      <tr v-bind:key="service">
+        <td>{{service.name}}</td>
+        <td>{{service.description}}</td>
+        <td>{{service.time}}</td>
+        <td>{{service.rate}}</td>
+        <td>{{service.limit}}</td>
+      </tr>
+    </template>
+  </table>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'ServiceList',
+  props: {
+    services: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ victor/src/components/masterDetail/ServiceMaster.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ServiceList></ServiceList>
+  </div>
+</template>
+
+<script>
+import ServiceList from './ServiceList'
+
+export default {
+  name: 'ServiceMaster',
+  data() {
+    return {
+      servics: [
+        {
+          name: ''
+        }
+      ]
+    }
+  },
+  components: [ServiceList]
+}
+</script>
+
+<style scoped>
+
+</style>
