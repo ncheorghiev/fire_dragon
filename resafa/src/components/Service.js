ee27ac1cd8e65bb67f40177040c499d8f2625054
Nicolai Gheorghiev <ngheorghiev@gmail.com>
2019-04-20 01:46:03 +0300

    Use object expand

--- resafa/src/components/Service.js
+++ resafa/src/components/Service.js
@@ -10,61 +10,36 @@ const service = (props) => {
                 <p>
                     <label>Name : </label>
                     <input type="text" name="name" value={props.serviceState.name} onChange={(event) => props.setService({
-                        id: props.serviceState.id,
-                        name: event.target.value,
-                        description: props.serviceState.description,
-                        timeType: props.serviceState.timeType,
-                        rate: props.serviceState.rate,
-                        limit: props.serviceState.limit,
-                        show: props.serviceState.show
+                        ...props.serviceState,
+                        name: event.target.value
                     })}/>
                 </p>
                 <p>
                     <label>Description : </label>
                     <input type="text" name="description" value={props.serviceState.description} onChange={(event) => props.setService({
-                        id: props.serviceState.id,
-                        name: props.serviceState.name,
-                        description: event.target.value,
-                        timeType: props.serviceState.timeType,
-                        rate: props.serviceState.rate,
-                        limit: props.serviceState.limit,
-                        show: props.serviceState.show
+                        ...props.serviceState,
+                        description: event.target.value
                     })}/>
                 </p>
                 <p>
                     <label>Time Type : </label>
                     <input type="text" name="timeType" value={props.serviceState.timeType} onChange={(event) => props.setService({
-                        id: props.serviceState.id,
-                        name: props.serviceState.name,
-                        description: props.serviceState.description,
-                        timeType: event.target.value,
-                        rate: props.serviceState.rate,
-                        limit: props.serviceState.limit,
-                        show: props.serviceState.show
+                        ...props.serviceState,
+                        timeType: event.target.value
                     })}/>
                 </p>
                 <p>
                     <label>Rate : </label>
                     <input type="text" name="rate" value={props.serviceState.rate} onChange={(event) => props.setService({
-                        id: props.serviceState.id,
-                        name: props.serviceState.name,
-                        description: props.serviceState.description,
-                        timeType: props.serviceState.timeType,
-                        rate: event.target.value,
-                        limit: props.serviceState.limit,
-                        show: props.serviceState.show
+                        ...props.serviceState,
+                        rate: event.target.value
                     })}/>
                 </p>
                 <p>
                     <label>Limit : </label>
                     <input type="text" name="limit" value={props.serviceState.limit} onChange={(event) => props.setService({
-                        id: props.serviceState.id,
-                        name: props.serviceState.name,
-                        description: props.serviceState.description,
-                        timeType: props.serviceState.timeType,
-                        rate: props.serviceState.rate,
-                        limit: event.target.value,
-                        show: props.serviceState.show
+                        ...props.serviceState,
+                        limit: event.target.value
                     })}/>
                 </p>
                 {props.serviceState.show === '2' && <p><button onClick={props.createService}>Create</button></p>}
