38d490e5eb1d02b7f5a7769c621313a69f7c1de4
Nicolai Gheorghiev <ngheorghiev@gmail.com>
2019-04-25 20:47:52 +0300

    Add component level style by using Radium

--- resafa/src/components/Books.js
+++ resafa/src/components/Books.js
@@ -1,18 +1,29 @@
 import React, {Component} from 'react'
 import '../App.css'
+import Radium from 'radium'
 
 class Books extends Component {
     // constructor(props) {
     //     super(props);
     // }
 
+
+
     render() {
+        const style = {
+            ':hover': {
+                backgroundColor: 'orange',
+                color: 'red',
+                cursor: 'pointer'
+            }
+        };
+
         return (
-            <div className="paddingDiv">
+            <div className="paddingDiv" style={style}>
                 This is just some books selection.
             </div>
         );
     }
 }
 
-export default Books;
+export default Radium(Books);
